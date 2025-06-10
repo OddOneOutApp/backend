390a2d3cdc2f8884aa249782c7b3f371d3f67c25
default <mail@greenman999.de>
2025-06-10 10:36:19 +0000

    feat: add generic RandomSelect function for random element selection

--- internal/utils/random/random.go
+++ internal/utils/random/random.go
@@ -22,3 +22,17 @@ func RandomStringWithCharset(length int, charset string) string {
 func RandomString(length int) string {
 	return RandomStringWithCharset(length, charset)
 }
+
+func RandomSelect[T any](input []T, count int) []T {
+	localRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	n := len(input)
+	if count >= n {
+		return input
+	}
+	indices := localRand.Perm(n)[:count]
+	result := make([]T, count)
+	for i, idx := range indices {
+		result[i] = input[idx]
+	}
+	return result
+}
